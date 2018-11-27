f4c4bf3fa172591d03e1ef5db22c5907a2cd2f01
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2018-11-27 19:56:38 +0100

    generate: Write output to directory

--- pxl/generate.py
+++ pxl/generate.py
@@ -12,8 +12,11 @@ def build(overview: state.Overview, output_dir: Path, template_dir: Path):
     album_template = load_template(template_dir / 'album.html.j2')
     photo_template = load_template(template_dir / 'photo.html.j2')
 
-    res = index_template.render(overview=overview)
-    print(res)
+    clear_directory(output_dir)
+    output_dir.mkdir()
+
+    with (output_dir / 'index.html').open('w+') as f:
+        index_template.stream(overview=overview).dump(f)
 
 
 def load_template(template_file: Path) -> jinja2.Template:
@@ -24,3 +27,18 @@ def load_template(template_file: Path) -> jinja2.Template:
 
     with template_file.open() as f:
         return jinja2.Template(f.read())
+
+
+def clear_directory(dir_path: Path):
+    if not dir_path.exists():
+        return
+
+    assert dir_path.is_dir()
+
+    for entry in dir_path.iterdir():
+        if entry.is_dir():
+            clear_directory(entry)
+        else:
+            entry.unlink()
+
+    dir_path.rmdir()
