a7735c1e5bc5349f3d7b64044ec3dee21326b7d7
Laurens Duijvesteijn <l.duijvesteijn@gmail.com>
2019-03-07 20:15:52 +0100

    design: Implement ESC to back to album
    Also, bind to the keyup event instead of down: binding on down means
    that we potentially mess with people's weird keybindings (e.g. mine).

--- design/js/photo.js
+++ design/js/photo.js
@@ -1,13 +1,15 @@
-// define constant keycode values for arrow keys
+// Define constant keycode values for navigation keys
+const ESCAPE = 27;
 const LEFT_ARROW = 37;
 const RIGHT_ARROW = 39;
 
-// bind navigation elementts
+// Bind navigation elementts. Back is for back to album.
+const back = document.getElementById("back");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
 
 // KeyPress -> Maybe ClickEvent
-document.onkeydown = (ev) => {
+document.onkeyup = (ev) => {
   switch(ev.keyCode)
   {
     case LEFT_ARROW:
@@ -15,6 +17,9 @@ document.onkeydown = (ev) => {
       break;
     case RIGHT_ARROW:
       if (next != null) { next.click(); }
+      break;
+    case ESCAPE:
+      if (back != null) { back.click(); }
       break;
   }
 }
