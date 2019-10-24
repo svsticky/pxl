679d3d0dcf4e1ed8b8d3e85c22fda52048b3fcb7
Hugo <hpeters1024.nl>
2019-10-24 20:28:51 +0200

    cli: fix overflowing cli message

--- pxl/cli.py
+++ pxl/cli.py
@@ -234,7 +234,7 @@ def deploy_cmd() -> None:
 @click.option("--force", is_flag=True, type=bool, help="Force break lock")
 def delete_cmd(album_name: str, force: bool) -> None:
     """
-    Delete an album from state file and remove its pictures from the file hosting.
+    Delete an album and its pictures.
     """
     cfg = config.load()
 
